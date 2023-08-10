deaecbd164b65d24174a22e45aebf520620b021b
tabaesso <tatabaesso@gmail.com>
2023-08-10 20:53:46 -0300

    Add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { Paragraph } from './styles';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -25,9 +26,9 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <Paragraph>
         Click on the Vite and React logos to learn more
-      </p>
+      </Paragraph>
     </>
   );
 }
