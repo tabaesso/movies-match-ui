06f49a17e6c266d294f76132dc6085a9df4970cf
tabaesso <tatabaesso@gmail.com>
2023-09-16 16:53:43 -0300

    Add query to share session page

--- src/pages/Session/share/index.tsx
+++ src/pages/Session/share/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import Button from '../../../components/Button';
 import { Container, Content } from '../../../components/styles';
+import { useQuery } from '@tanstack/react-query';
+import api from '../../../services/api';
+import LoadingOverlay from '../../../components/LoadingOverlay';
+import { toast } from 'react-toastify';
 
 const ShareSession = () => {
   const { id } = useParams<{ id: string }>();
@@ -12,16 +16,27 @@ const ShareSession = () => {
     navigate(`/session/${id}`);
   };
 
-  // TODO: get session by id
+  const { isLoading, error, data, isFetching } = useQuery(['currentSession'], () =>
+    api.get(
+      `/sessions/${id}`
+    ).then((res) => res.data)
+  );
+
+  React.useEffect(() => {
+    if (!error) return;
+
+    toast.error('Erro ao carregar sessão');
+  }, [error]);
 
   return (
     <Container>
       <Link to='/home'>Voltar</Link>
+      <LoadingOverlay isLoading={isLoading || isFetching} />
       <Content>
         <>
           <h3>Compartilhe o link com seus amigos!</h3>
-          <input type="text" value={`https://tabaesso.com/session/${id}`} readOnly />
-          <h4>Entraram na {id}:</h4>
+          <input type="text" value={`${import.meta.env.VITE_BASE_URL}/session/${id}`} readOnly />
+          <h4>Entraram na {data.name}:</h4>
           <ul>
             <li>Usuário 1</li>
             <li>Usuário 2</li>
